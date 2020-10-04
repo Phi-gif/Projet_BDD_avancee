c5772f5c9d8fee4ce0eae0eef7f048fa2bb5c617
elvinaeury <61745885+elvinaeury@users.noreply.github.com>
2020-10-04 18:03:48 +0200

    Add files via upload

---
+++ Problème_multi_threading/Elvi/Partie1_Question1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 14:14:51 2020
+
+@author: elvinagovendasamy
+"""
+
+# =============================================================================
+# Question 1
+# =============================================================================
+import random
+
+class Functions():
+    def __init__(self,x):
+        self.x=x
+        
+    def __dir__(self):
+        return['f1','f2','f3']
+    
+    def f1(self):
+        return (self.x+3)/2
+    
+    def f2(self):
+        return self.x
+    
+    def f3(self):
+        return self.x+5
+    
+    def random_function(self,instructions):
+        
+# Working test from list
+# =============================================================================
+#         my_functs=[self.f1,self.f2]  
+#         result=random.choice(my_functs)()
+#         return result
+# =============================================================================
+# Generalization:
+        values=[self.f1(),self.f2(),self.f3()]
+        keys=[i for i in dir(Functions(self.x)) if not i.startswith("__")] #ok
+        init_dico=dict(zip(keys,values))
+        
+        liste_f=[]
+        for item in instructions:
+            if item in init_dico:
+                liste_f.append([item,init_dico[item]])
+                
+        return random.choice(liste_f)
+            
+
+
+
+if __name__=='__main__':
+    y=2
+    myFunction=Functions(y)
+    instruction_list=['f1','f2']
+    my_result=myFunction.random_function(instruction_list)
+    print('la fonction et sa valeur sont : ',my_result)
+
+
+
+
