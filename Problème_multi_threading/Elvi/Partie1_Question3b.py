91bb3a40cb84609f9e304277d4b76eb8c0892bc0
elvinaeury <61745885+elvinaeury@users.noreply.github.com>
2020-10-07 23:17:38 +0200

    Add files via upload

---
+++ Problème_multi_threading/Elvi/Partie1_Question3b.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 14:14:51 2020
+
+@author: elvinagovendasamy
+"""
+
+# =============================================================================
+# Question 2
+# =============================================================================
+import random
+
+class Functions():
+    def __init__(self):
+        pass
+        
+    def read_file(self):
+        f=open('liste_fonctions.txt','r')
+        instruction_list=[]
+        for lines in f:
+            instruction_list.append(lines.strip('\n'))
+        f.close()
+        return instruction_list
+        
+  
+    
+    def run_functions(self):
+        dico={}
+        instruction_list=Functions.read_file(self)
+        random.shuffle(instruction_list)
+        
+        n=len(instruction_list)
+        for i in range(n):
+            chosen_function=instruction_list[i]
+
+            if eval(chosen_function)>100:
+                dico[chosen_function]=ValueTooLarge(self)
+            else:
+                dico[chosen_function]=eval(chosen_function)
+
+        return dico
+            
+
+
+class ValueTooLarge(Exception):
+    def __init__(self,number,message="Nous cherchons un résultat inférieur à 100, en choisir une autre!"):
+        self.number=number
+        self.message=message
+        super().__init__(self.message)
+
+
+
+if __name__=='__main__':
+
+    f=Functions()
+    result=f.run_functions()
+
+    print(result)
+
+
+
+
